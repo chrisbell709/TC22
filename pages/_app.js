761609a3a2d2d54da6deb87ae16d041fd489d515
chrisbell709 <chrisbell709@gmail.com>
2025-03-09 20:44:24 -0400

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import 'https://github.com/chrisbell709/TC22/blob/main/styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
+
+https://github.com/chrisbell709/TC22/blob/main/styles/globals.css
